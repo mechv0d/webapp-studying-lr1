3e2997b6969eac62950c71469090bd6a8c6226ab
mechv0d <sergeireim06@yandex.ru>
2025-09-24 13:01:08 +0700

    add GET

--- src/services/api.js
+++ src/services/api.js
@@ -1,12 +1,16 @@
-// src/services/api.js
 import axios from 'axios';
 
-// Создаем экземпляр axios с предварительной настройкой
+// axios object
 const apiClient = axios.create({
-    baseURL: 'https://jsonplaceholder.typicode.com', // Базовый URL для всех запросов
+    baseURL: 'https://jsonplaceholder.typicode.com', // base url
     headers: {
-        'Content-Type': 'application/json', // Указываем, что мы отправляем/получаем JSON
+        'Content-Type': 'application/json',
     },
 });
+
+export const getPosts = async () => {
+    const response = await apiClient.get('/posts');
+    return response.data;
+};
 
 export default apiClient;
